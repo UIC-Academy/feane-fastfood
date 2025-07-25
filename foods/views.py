5f28afa27a9189dea933aa45135be8be1b8450d2
Samandar-Komilov <komilov.samandar07@gmail.com>
2025-07-25 21:04:53 +0500

    basic template context

--- foods/views.py
+++ foods/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from foods.models import Category
+
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        categories = Category.objects.all()
+        kwargs["categories"] = categories
+        return super().get_context_data(**kwargs)
 
 class MenuView(TemplateView):
     template_name = "menu.html"
